1597ca81ad1fe9e95b4511124b8026befee47fa4
Santana <mauriciosantanadasilva123@gmail.com>
2026-04-06 12:26:28 +0000

    add

---
+++ minhasClasses/Aluno.java
@@ -0,0 +1,16 @@
+package EstudosJava.minhasClasses;
+
+class Aluno{
+    private String nome;
+    private int matricula;
+    private char sexo;
+
+    public Aluno(){        
+    }
+
+    public Aluno(String nome, int matricula, char sexo){        
+        this.nome = nome;
+        this.matricula = matricula;
+        this.sexo = sexo;
+    }
+}
---
+++ minhasClasses/Teste.java
@@ -0,0 +1,5 @@
+public class Teste{
+    static public void main (){
+        System.out.println("Olá Mundo");
+    }
+}
